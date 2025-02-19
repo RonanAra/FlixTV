b8e389502e120c36fcf7e9017f821fc10f9a1dc8
Ronan Fernandes <ronanlm10@hotmail.com>
2025-02-19 11:44:24 -0300

    feat: Added hilt annotation in activity

--- app/src/main/java/br/com/tvflix/presentation/ui/MainActivity.kt
+++ app/src/main/java/br/com/tvflix/presentation/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package br.com.tvflix.presentation.ui
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import br.com.tvflix.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
